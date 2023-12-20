e54d601ba96d2392f8cad6207152b8149549346d
xieweijia <17858452881@163.com>
2023-12-20 17:54:53 +0800

    data = "9"

--- BCC.py
+++ BCC.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 def bcc_checksum(data):
     """这是一个异或校验的函数"""
 
@@ -12,12 +13,9 @@ def bcc_checksum(data):
 file_path = Path("set_resistance.txt")
 data_1 = [0x06, 0x02, 0x09, 0x00, 0x00]
 
-checksum = bcc_checksum(data_1)
+
+# checksum = bcc_checksum(data_1)
 # print(f"BCC checksum: 0x{checksum:02X}")
-# https://blog.51cto.com/u_16175438/6865399
-print(f"BCC checksum: 0x{260:02X}")
-# 存放指令
-set_resistance = ''
 
 levels = [level*10 for level in range(1, 34)]
 for lev in levels:
@@ -27,12 +25,29 @@ for lev in levels:
         checksum = bcc_checksum(data_1)
         # 打印校验码
         # print(f"{int(lev/10)} BCC checksum: 0x{checksum:02X}")
-        # 组成完整的指令
-        set_resistance = f'{int(lev/10)} 06020900{lev:02X}{checksum:02X}'
+        # 将校验码组成完整的指令
+        set_resistance = f'{int(lev/10)} AA060209{lev:02X}00{checksum:02X}55'
         # 以追加模式打开
         with file_path.open('a') as file_obj:
             file_obj.write(set_resistance + "\n")
-        # print(set_resistance)
     else:
-        print(f"BCC checksum: 0x{lev:02X}")
+        # print(lev)
+        # print(f"{lev:02X}")
+        lev_16 = f"{lev:02X}"
+        high_2 = '0' + lev_16[:1]
+        low_2 = lev_16[1:]
+        # print(low_2, high_2)
+        data_1[3] = int(low_2, 16)  # 16进制转成十进制
+        data_1[4] = int(high_2, 16)
+        # print(data_1)
 
+        # 异或校验，生成checksum
+        checksum = bcc_checksum(data_1)
+        # 打印校验码
+        print(f"{int(lev/10)} BCC checksum: 0x{checksum:02X}")
+
+        # 将校验码组成完整的指令
+        set_resistance = f'{int(lev/10)} AA060209{low_2}{high_2}{checksum:02X}55'
+        # 以追加模式打开
+        with file_path.open('a') as file_obj:
+            file_obj.write(set_resistance + "\n")

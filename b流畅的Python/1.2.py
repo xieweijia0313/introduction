6dc26d923342000fa4eca337ccc9a0cb38fa6eca
xieweijia <17858452881@163.com>
2024-01-03 15:50:31 +0800

    data = "14"

--- b流畅的Python/1.2.py
+++ b流畅的Python/1.2.py
@@ -31,8 +31,10 @@ class FrenchDeck:
 beer_card = Card('7', 'diamonds')
 
 deck = FrenchDeck()
+print(deck.__getitem__(0))
+
 # print(len(deck))
-# print(deck[0])  # 抽取第一张
+print(deck[0])  # 抽取第一张
 # print(choice(deck))  # 随机抽一张
 
 # 迭代纸牌
@@ -46,11 +48,11 @@ deck = FrenchDeck()
 suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 
 
-def spades_high(card):
-    # 检测字符串中是否包含子字符串card.rank
-    rank_value = FrenchDeck.ranks.index(card.rank)
-    return rank_value * len(suit_values) + suit_values[card.suit]
-
-
-for card in sorted(deck, key=spades_high):
-    print(card)
+# def spades_high(card):
+#     # 检测字符串中是否包含子字符串card.rank
+#     rank_value = FrenchDeck.ranks.index(card.rank)
+#     return rank_value * len(suit_values) + suit_values[card.suit]
+#
+#
+# for card in sorted(deck, key=spades_high):
+#     print(card)
